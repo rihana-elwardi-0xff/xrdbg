ccb1cec63e479c69d212e82b3c1b8d9d54acbe3b
Rihana's <87090371+rihana-elwardi-0xff@users.noreply.github.com>
2022-05-18 20:03:09 +0100

    Update rihana-rdb-google-creater.sh

--- rihana-rdb-google-creater.sh
+++ rihana-rdb-google-creater.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Rihana's0xff Setup RDB ... " >&2
 {
-sudo useradd -m rihana\'s
-sudo adduser rihana\'s sudo
-echo 'rihana\'s:8426' | sudo chpasswd
+sudo useradd -m rihana0xff
+sudo adduser rihana0xff sudo
+echo 'rihana0xff:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser CyberCos chrome-remote-desktop
+sudo adduser rihana0xff chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - CyberCos -c """$CRP"""
+su - rihana0xff -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
